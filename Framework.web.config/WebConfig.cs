eb74f246accf0b693102b7c6e963df4bed2c5a22
yangyukun <yangyukun@live.com>
2016-08-08 22:10:22 +0800

    initproject

---
+++ Framework.web.config/WebConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Xml;
+
+namespace Framework.web.config
+{
+    public class WebConfig
+    {
+
+    }
+}
---
+++ Framework.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Framework.web.Startup))]
+namespace Framework.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
